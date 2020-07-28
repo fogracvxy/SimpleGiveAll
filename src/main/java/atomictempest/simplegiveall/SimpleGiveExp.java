b6ff549e01e980fcc774b1a27a87ff7c2553b20b
fogracvxy <marn1002000@gmail.com>
2020-07-28 14:06:37 +0200

    Improved targeting

--- src/main/java/atomictempest/simplegiveall/SimpleGiveExp.java
+++ src/main/java/atomictempest/simplegiveall/SimpleGiveExp.java
@@ -1,5 +1,6 @@
 package atomictempest.simplegiveall;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -14,12 +15,14 @@ public SimpleGiveExp (SimpleGiveAll plugin){
 }
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        String potentPlayer = args[0];
         if (command.getName().equalsIgnoreCase("sgiveexp")){
-            Player p = (Player) sender;
+            Player p = Bukkit.getServer().getPlayer(potentPlayer);
             if(p.isOnline()){
                 p.giveExp(2000,false);
+                p.sendMessage(ChatColor.GREEN + "You gained 2000 exp");
 
-            }else {
+            }else  {
                 p.sendMessage(ChatColor.RED + "Player is not online");
             }
         }
